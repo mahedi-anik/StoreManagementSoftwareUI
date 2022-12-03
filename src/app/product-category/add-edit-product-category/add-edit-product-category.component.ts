fdab7ea3f58081963bde74aa3bd334a1b361120b
mahedi-anik <65283345+mahedi-anik@users.noreply.github.com>
2022-12-03 18:20:26 +0600

    12-3-2022

--- src/app/product-category/add-edit-product-category/add-edit-product-category.component.ts
+++ src/app/product-category/add-edit-product-category/add-edit-product-category.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,Input } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
+import { MatDialogRef } from '@angular/material';
+import { ProductCategoryComponent } from '../product-category.component';
+
 
 @Component({
   selector: 'app-add-edit-product-category',
   templateUrl: './add-edit-product-category.component.html',
   styleUrls: ['./add-edit-product-category.component.scss']
 })
-export class AddEditProductCategoryComponent {
+export class AddEditProductCategoryComponent implements OnInit {
+constructor(private service: SharedService,public dialogRef: MatDialogRef<ProductCategoryComponent>){}
+  
+  @Input() 
+  productCategory:any;
+  ProductCategoryId:string;
+  ProductCategoryName:string;
+  ProductCategoryDescription:string;
+  EntryBy:string;
+  ProductCategoryList:any=[];
+  index:number = 0;
+  ModelTitle!:string;
+
+
+
+  ngOnInit(): void {
+    this.LoadProductCategoryList();
+  }
+  LoadProductCategoryList(){
+    this.service.getAllProductCategoryList().subscribe((data:any)=>{
+      this.ProductCategoryId=this.productCategory.ProductCategoryId;
+      this.ProductCategoryName=this.productCategory.ProductCategoryName;
+      this.ProductCategoryDescription=this.productCategory.ProductCategoryDescription;
+      this.EntryBy=this.productCategory.EntryBy;
+    });
+  }
+
+  onClose() {
+    // this.service.form.reset();
+    this.dialogRef.close();
+  }
+
+
+
+
+
+
+
+
 
 }
--- src/app/product-category/show-product-category/show-product-category.component.ts
+++ src/app/product-category/show-product-category/show-product-category.component.ts
@@ -1,7 +1,12 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,OnInit,ViewChild  } from '@angular/core';
 import {MatDialog,MatDialogConfig} from '@angular/material/dialog';
-import { AddEditCustomerComponent } from 'src/app/customer/add-edit-customer/add-edit-customer.component';
+import { SharedService } from 'src/app/shared.service';
+import {faPenToSquare,faTrashCan} from '@fortawesome/free-solid-svg-icons'
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
 import { AddEditProductCategoryComponent } from '../add-edit-product-category/add-edit-product-category.component';
+import { MatTableDataSource } from '@angular/material/table';
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-show-product-category',
   templateUrl: './show-product-category.component.html',
@@ -10,23 +15,69 @@ import { AddEditProductCategoryComponent } from '../add-edit-product-category/ad
 
 export class ShowProductCategoryComponent implements OnInit {
 
-
-  constructor(private dialog: MatDialog){}
-  
+  constructor(private dialog: MatDialog,private service:SharedService,private datePipe: DatePipe){}
+  faPenToSquare=faPenToSquare;
+  faTrashCan=faTrashCan;
   ProductCategoryList:any=[];
+  ProductCategorydataSource;
+  productCategory:any;
+  displayedColumns: string[] = ['ProductCategoryId', 'ProductCategoryName', 'ProductCategoryDescription','Actions'];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
 
   ngOnInit(): void {
-    
+    this.refreshProductCategoryList();
+  }
+  refreshProductCategoryList()
+  {
+    this.service.getAllProductCategoryList().subscribe(data=>{
+      this.ProductCategoryList=data;
+      this.ProductCategorydataSource=new MatTableDataSource(this.ProductCategoryList);
+      this.ProductCategorydataSource.paginator=this.paginator;
+      this.ProductCategoryList.sort=this.sort;
+    });
+  }
+  applyFilter(event: Event){
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.ProductCategorydataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.ProductCategorydataSource.paginator) {
+      this.ProductCategorydataSource.paginator.firstPage();
+    }
+  }
+  addProductCategory()
+  {
+    this.productCategory={
+      ProductCategoryId:0,
+      ProductCategoryName:"",
+      ProductCategoryDescription:"",
+      EntryBy:"",
+      EntryDate:this.datePipe.transform(new Date(), 'yyyy-MM-dd')
+    }
+    const dialogConfig=new MatDialogConfig();
+    dialogConfig.disableClose=true;
+    dialogConfig.autoFocus=true;
+    dialogConfig.width="60%";
+    this.dialog.open(AddEditProductCategoryComponent,dialogConfig);
   }
 
-  openDialog() {
 
-    const dialogConfig = new MatDialogConfig();
+// editShelf(item:any){
+//   this.shelf=item;
+//   this.ModelTitle="Edit Shelf Info!";
+//   this.ActivateAddEditShelfComp=true;
+// }
 
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
 
-    this.dialog.open(AddEditProductCategoryComponent, dialogConfig);
+deleteProductCategory(id:any)
+{
+  if (confirm('Are you sure??')==true) {
+    
+    this.service.deleteProductCategory(id).subscribe((data:any)=>{
+      this.refreshProductCategoryList();
+    });
+  }
 }
 
 
